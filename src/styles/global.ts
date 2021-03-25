7edf7ecd33dbbdcdb1d039efde6c7d03e8f92761
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-24 22:13:03 -0300

    sanet company

---
+++ src/styles/global.ts
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle `
+    :root{
+        --background: #FAF8FE;
+
+        --purple: #280063;
+        --purple-dark: #240059;
+        --purple-light: #442279;
+
+        --yellow: #FFCF15;
+        --yellow-dark: #C2A60A;
+        --yellow-light: #FFD731;
+
+        --gray: #89898B;
+        --gray-dark: #474749;
+        --gray-light: #CBCBCC;
+        
+        --black: #1B0043;
+        --black-light: #210052;
+        --black-dark: #100025;   
+    }
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html {
+        // 1REM = 16px
+
+        @media (max-width: 1080px) {
+            font-size: 93.75%; // 15px
+        }
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px
+        }
+    }
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+    body, input, textarea, button {
+        font-family: 'Sora', sans-serif;
+        font-weight: 400;
+    }
+    h1, h2, h3, h4, h5, h6, strong {
+        font-family: 'Sora', sans-serif;
+        font-weight: 600;
+    }
+    p {
+        font-family: 'Source Sans Pro', sans-serif;
+    }
+    button {
+        cursor: pointer;
+    }
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+    
+
+
+`

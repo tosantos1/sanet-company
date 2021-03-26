4a8c456e60ed88a401aaa5518a7e283224e4be6e
Tiago Oliveira <tiagolivesan@outlook.com>
2021-03-26 18:12:57 -0300

    sanet company

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,47 @@
+import logoImg from '../../assets/logo.svg'
+import logotipoImg from '../../assets/logotipo.svg'
+import { Container } from './styles'
+
 export function Header(){
     return (
-        <header>
-            Header
-        </header>
+        <Container>
+            {/* HEADER DESKTOP */}
+            <nav className="navbar-desktop">
+                <div className="item-1">
+                    <a href=""><img src={logoImg} alt="logo"/></a>
+                </div>
+                <div className="item-2">
+                    <ul className="navlist-desktop">
+                        <li><a href="" className="navlink-desktop">Inicio</a></li>
+                        <li><a href="" className="navlink-desktop">Vantagens</a></li>
+                        <li><a href="" className="navlink-desktop">Planos</a></li>
+                        <li><a href="" className="navlink-desktop">Contato</a></li>
+                        <li><a href="" className="navlink-desktop">Assinante</a></li> 
+                    </ul>
+                </div>
+            </nav>
+
+            {/* HEADER MOBILE */}
+            <nav className="navbar-mobile">
+                <div className="logo-m">
+                    <a href=""><img src={logotipoImg} alt="logo"/></a>
+                </div>
+                <div className="bx" id="bx"></div>
+            </nav>
+
+            {/* HEADER MENU */}
+            <nav className="menu-mobile">
+                <ul className="navlist-mobile">
+                    <li><a href="" className="navlink-mobile">Inicio</a></li>
+                    <li><a href="" className="navlink-mobile">Vantagens</a></li>
+                    <li><a href="" className="navlink-mobile">Planos</a></li>
+                    <li><a href="" className="navlink-mobile">Contato</a></li>
+                    <li><a href="" className="navlink-mobile">Assinante</a></li>  
+                </ul>
+            </nav>
+
+            
+
+        </Container>
     )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,126 @@
+import styled from 'styled-components'
+
+export const Container = styled.header `
+    .menu-mobile {
+        display: none;
+    }
+    .navbar-desktop {
+        margin: 0 auto;
+        max-width: 1200px;
+        padding: 1rem 1.5rem;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .item-2{
+        }
+        
+        .navlist-desktop{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 1rem 1.4rem;
+            list-style: none;
+        }
+        .navlink-desktop {
+            text-decoration: none;
+            margin: 0.7rem 1.3rem;
+            
+            position: relative;
+            
+            &::after {
+                content: "";
+                position: absolute;
+
+                top: 80%;
+                left: 0px;
+
+                width: 0%;
+                height: 3px;
+                
+                transition: 1s ease;
+                
+                background-color: #FFCF15;
+            }
+            &:hover {
+
+                 &::after{
+                width: 105%;
+            }
+            }
+        }
+        li:nth-child(5){
+            cursor: pointer;
+            border: 2px solid #240059;
+            border-radius: 1rem;
+            padding: 0.8rem 1.5rem;
+
+            transition: ease-in-out 1s;
+
+            :hover {
+                    background-color: #FFCF15;
+            }
+        }
+    }
+    @media (max-width: 1050px) {
+        .navbar-desktop {
+            display: none;
+        }
+    }
+    .navbar-mobile {
+        display: none;
+        border: 1px solid red;
+        padding: 1rem;
+
+        justify-content: space-between;
+        align-items: center;
+    }
+    .logo-m {
+        border: 1px solid red;
+    }
+    .bx {
+        border: 1px solid black;
+        width: 36px;
+        height: 36px;
+
+        position: relative;
+        cursor: pointer;
+
+        z-index: 999;
+        transition: .7s ease;
+
+        &::after {
+            content: '';
+            position: absolute;
+            top: 40%;
+            left: 17%;
+
+            width: 70%;
+            height: 2px;
+            border-radius: 1rem;
+
+            background: var(--purple);
+        }
+        &::before {
+        content: '';
+        position: absolute;
+        top: 64%;
+        left: 17%;
+
+        width: 70%;
+        height: 2px;
+
+        transition: .7s ease;
+
+        background-color: var(--purple);
+        }
+        
+    }
+
+    @media (max-width: 1050px) {
+            .navbar-mobile{
+                display: flex;
+            }
+        }
+
+`

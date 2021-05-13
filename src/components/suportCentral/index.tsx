57d1706230d81fb528952840bc46de526eef2a3e
Calixoto <grimmjow006@live.com>
2021-05-13 12:55:07 -0300

    edit5

--- src/components/suportCentral/index.tsx
+++ src/components/suportCentral/index.tsx
@@ -6,10 +6,8 @@ export function SuportCentral() {
             <div id="box-suport">
                 <h1>CENTRAL DE SUPORTE</h1>
                 <form action="">
-                    <label htmlFor="nome">Nome</label>
-                    <input type="text" name="name" id="nome" />
-                    <label htmlFor="tel">Celular</label>
-                    <input type="tel" name="tel" id="tel" />
+                    <input type="text" name="name" id="nome" value="Nome" />
+                    <input type="tel" name="tel" id="tel" value="Celular" />
                     <textarea name="msg" rows={15} cols={73} >Mensagem</textarea>
                     <input type="submit" id="submitForms" value="Enviar" />
                 </form>
